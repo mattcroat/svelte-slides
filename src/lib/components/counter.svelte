c5ce1edb1fdfd1b00b2078dc8826c3073cc38845
mattcroat <mattcroat.dev@gmail.com>
2022-06-18 16:28:55 +0200

    chore: Add counter example

---
+++ src/lib/components/counter.svelte
@@ -0,0 +1,25 @@
+<script>
+  let count = 0
+</script>
+
+<div class="counter">
+  <button on:click={() => (count -= 1)}>-</button>
+  <span>{count}</span>
+  <button on:click={() => (count += 1)}>+</button>
+</div>
+
+<style>
+  .counter {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  button {
+    padding: 0.6rem 1rem;
+    font-weight: 700;
+    border: none;
+    border-radius: 20px;
+  }
+</style>
